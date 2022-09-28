20d4d6e4e82e444be5327f173385e52b956ef625
ERKELSOT <1256029807@qq.com>
2022-09-28 22:00:33 +0800

    chore(pm2): update pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,13 +2,13 @@ module.exports = {
   apps: [
     {
       name: 'erkelost',
-      script: 'dist/src/main.js',
-      env_production: {
-        RUNNING_ENV: 'prod',
-      },
-      env_development: {
-        RUNNING_ENV: 'dev',
-      },
+      script: 'dist/main.js',
+      // env_production: {
+      //   RUNNING_ENV: 'prod',
+      // },
+      // env_development: {
+      //   RUNNING_ENV: 'dev',
+      // },
     },
   ],
 };
