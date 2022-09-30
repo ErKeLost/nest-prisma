a2dd5a83f556d817b89727f3ca5ae5266436ee36
erkelost <1256029807@qq.com>
2022-09-30 11:55:57 +0800

    chore(service): update service

--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -3,7 +3,7 @@ import { IsEmail, IsNotEmpty, IsString } from 'class-validator'
 export class AuthDto {
   @ApiProperty()
   @IsEmail()
-  // @IsNotEmpty()
+  @IsNotEmpty()
   email: string
 
   @ApiProperty({ required: false })
